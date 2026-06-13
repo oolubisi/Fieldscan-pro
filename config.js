2f3258dd5086bafb73b41700a3e3aada1328c3b6
oolubisi <oolubisi@gmail.com>
2026-06-13 14:00:53 +0100

    Add configuration constants for GAS_URL and AUTH_TOKEN

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+export const GAS_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"; // REPLACE
+export const AUTH_TOKEN = "FieldScan2025!SecureToken";
+export const ATTACHMENT_DELIMITER = "|||";
