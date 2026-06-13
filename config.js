16f1ecb9ad7839caee4839a77a7ebe4397db33df
oolubisi <oolubisi@gmail.com>
2026-06-13 14:01:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-export const GAS_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"; // REPLACE
+export const GAS_URL = "hhttps://script.google.com/macros/s/AKfycbyMbq7heDDK6lelr4fD3ek24njHQDmo7zaC8ocusLGDyu4u-NxoO7e_Wt-zgaRkgIbJ/exec"; // REPLACE
 export const AUTH_TOKEN = "FieldScan2025!SecureToken";
 export const ATTACHMENT_DELIMITER = "|||";
