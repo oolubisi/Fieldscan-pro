ab589c701f1a84d294d5b8d9561824150bec799b
oolubisi <oolubisi@gmail.com>
2026-06-14 08:53:38 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-export const GAS_URL = "https://script.google.com/macros/s/AKfycbyYaH5dTCC7u-ZYosJBdRwxp7_-V5jS0zFSLbWGtIPNSIG98ZLQZL29h2-KHthEcleA/exec"; // REPLACE
-export const AUTH_TOKEN = "FieldScan2025!SecureToken";
-export const ATTACHMENT_DELIMITER = "|||";
