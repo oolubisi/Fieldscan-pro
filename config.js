230280f7d83fd709faa2164242ab943fbedd286a
oolubisi <oolubisi@gmail.com>
2026-06-14 07:17:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-export const GAS_URL = "https://script.google.com/macros/s/AKfycbyMbq7heDDK6lelr4fD3ek24njHQDmo7zaC8ocusLGDyu4u-NxoO7e_Wt-zgaRkgIbJ/exec"; // REPLACE
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbw9Nf3onUIGiSzbGpIEsMy1R7CixYlbbZaAtwyLnkUmwvzvg8EbG2Hd87j7ZteFHoQ-/exec"; // REPLACE
 export const AUTH_TOKEN = "FieldScan2025!SecureToken";
 export const ATTACHMENT_DELIMITER = "|||";
