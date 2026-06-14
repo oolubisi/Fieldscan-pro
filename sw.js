77c960007ef4773316b9b48e73fdc9cb4b0fe48b
oolubisi <oolubisi@gmail.com>
2026-06-14 09:35:14 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "fieldscan-v3";
-const ASSETS = ["./", "./index.html", "./style.css", "./script.js", "./manifest.json"];
+const CACHE_NAME = "fieldscan-v4";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.bundle.js", "./manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))));
 self.addEventListener("fetch", e => {
